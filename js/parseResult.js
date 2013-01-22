47eec346be4c56bee336b1ee5ca557e9d54463a4
Alex Skorulis <skorulis@gmail.com>
2013-01-22 22:52:17 +0000

    Work on parserState editing

--- js/parseResult.js
+++ js/parseResult.js
@@ -1,14 +1,24 @@
 bb.parseResult = {};
-bb.parseResult.cell = '<div class="parseResult">'
-	+'<p class="pr_title">titlesdf sdafu ghsdo fgsdo ufguisd gfiu </p>'
-	+'<p class="pr_unknown">Unknown words:</p>'
-	+'</div>';
+bb.parseResult.cell = '<div class="parseResult"><p class="pr_title">TITLE</p><p class="pr_result">RESULT</p><p class="pr_unknown">Unknown words:</p></div>';
+
+bb.parseResult.unknownWord = '<a href="google.com">TEST</a>';
 
 
 bb.parseResult.generateCell = function(result) {
 	var ret = jQuery(bb.parseResult.cell);
 	console.log(result);
-	ret.find(".pr_title").text("" + result.input);
+	ret.find(".pr_title").text(result.input);
+	ret.find(".pr_result").text(result.result);
+	if(result.result=="SUCCESS") {
+		ret.addClass("pr_success");
+	} else {
+		ret.addClass("pr_failure");
+	}
+	result.unknownWords.forEach(function(element) {
+		var html = '<a href="google.com" onclick="unknowClicked()">' + element + '</a>'; 
+		var item = jQuery(html);
+		ret.append(item);
+	});
 	return ret;
 };
 
---
+++ js/parserState.js
@@ -0,0 +1,10 @@
+bb.ps = {};
+
+bb.ps.addTerm = function(item) {
+	var parent = $(".parseResult").find(".cell_title");
+	console.log(item);
+};
+
+bb.ps.removeTerm = function(item) {
+	
+};
