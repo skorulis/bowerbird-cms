ba6dce3346225ea0e053eda6e6ea4be3b2f9a93d
Alex Skorulis <skorulis@gmail.com>
2013-01-24 19:42:26 +0000

    Experiments

--- js/parserState.js
+++ js/parserState.js
@@ -1,11 +1,15 @@
 bb = bb || {};
 bb.ps = {};
 
-bb.ps.addTerm = function(item) {
-	var parent = $(".parseResult").find(".cell_title");
-	console.log(item);
+bb.ps.termCell = '<li><input type="text" class="ps_term" /><a class="ps_addButton ui-state-default ui-corner-all" onclick="bb.ps.removeTerm(this)"><span class="ui-icon ui-icon-circle-minus"></span></a></li>';
+
+bb.ps.addTerm = function() {
+	var parent = $(".parseResult").find("ul.ps_term_list");
+	console.log(parent.children().length);
+	var newCell = $(bb.ps.termCell);
+	parent.append(newCell);
 };
 
-bb.ps.removeTerm = function(item) {
-	
+bb.ps.removeTerm = function(itemNumber) {
+	console.log(item);
 };
